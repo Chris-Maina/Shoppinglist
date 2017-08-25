308118c497ba4ed7b1848720ae1e72420e4c861d
Chris-Maina <krismaish34@gmail.com>
2017-08-25 22:47:29 +0300

    [Update]views.py to add routes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 """ views.py """
-from flask import render_template, request
+from flask import render_template
 from app import app
 
 @app.route('/')
@@ -7,3 +7,27 @@ def index():
     """Handles rendering of index page
     """
     return render_template("index.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Handles registeration of users
+    """
+    return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Handles logging in
+    """
+    return render_template("login.html")
+
+@app.route('/shoppinglist', methods=['GET', 'POST'])
+def shoppinglist():
+    """Handles shopping list creation
+    """
+    return render_template('shoppinglist.html')
+
+@app.route('/shoppingitems', methods=['GET', 'POST'])
+def shoppingitems():
+    """Handles shopping items creation
+    """
+    return render_template('shoppingitems.html')
