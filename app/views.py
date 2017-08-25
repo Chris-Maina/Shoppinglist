4c3ed8362a6fb0db9f51999e11e1b05c8b35491e
Chris-Maina <krismaish34@gmail.com>
2017-08-25 23:43:43 +0300

    [Update]views.py modify register() login()

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 """ views.py """
-from flask import render_template
-from app import app
+from flask import render_template, request, session
+from app import app, user_object
 
 @app.route('/')
 def index():
@@ -12,12 +12,34 @@ def index():
 def register():
     """Handles registeration of users
     """
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        cpassword = request.form['confirm-password']
+
+        msg = user_object.registeruser(username, email, password, cpassword)
+        if msg == "Successfully registered. You can now login!":
+            return render_template("login.html", resp=msg)
+        else:
+            return render_template("register.html", resp=msg)
+
     return render_template("register.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """Handles logging in
     """
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        msg = user_object.login(email, password)
+        if msg == "Successfully logged in, create buckets!":
+            session['email'] = email
+            session['logged_in'] = True
+            return render_template('shoppinglist.html', resp=msg)
+        else:
+            return render_template('login.html', resp=msg)
     return render_template("login.html")
 
 @app.route('/shoppinglist', methods=['GET', 'POST'])
