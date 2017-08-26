6366808ff3976c4f4fa749c5edcc688c3ad5975e
Chris-Maina <krismaish34@gmail.com>
2017-08-26 09:48:27 +0300

    [Update]__init__.py add shoppinglist import

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 """ app/__init__.py """
 
 from flask import Flask
-from app import useraccounts
+from app import useraccounts,shoppinglist
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 app.secret_key = 'dresscodesleepbehappy'
 
 user_object = useraccounts.UserClass()
+shoplist_obj = shoppinglist.ShoppinglistClass()
 # Load the views
 from app import views
 
