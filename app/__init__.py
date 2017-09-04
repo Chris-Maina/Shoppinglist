2863ea6c9627812c6694b88143c866c9a2db33c1
Chris-Maina <krismaish34@gmail.com>
2017-09-04 23:02:07 +0300

    [Refactor]__init__.py code

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@ from flask import Flask
 from app import useraccounts, shoppinglist, shoppingitems
 
 # Initialize the app
-app = Flask(__name__, instance_relative_config=True)# pylint: disable=invalid-name
+app = Flask(  # pylint: disable=invalid-name
+    __name__, instance_relative_config=True)
 app.secret_key = 'dresscodesleepbehappy'
 
-user_object = useraccounts.UserClass()# pylint: disable=invalid-name
-shoplist_obj = shoppinglist.ShoppinglistClass()# pylint: disable=invalid-name
-shopitems_obj = shoppingitems.ShoppingItemsClass()# pylint: disable=invalid-name
+user_object = useraccounts.UserClass()  # pylint: disable=invalid-name
+shoplist_obj = shoppinglist.ShoppinglistClass()  # pylint: disable=invalid-name
+shopitems_obj = shoppingitems.ShoppingItemsClass()  # pylint: disable=invalid-name
 # Load the views
 from app import views
 
