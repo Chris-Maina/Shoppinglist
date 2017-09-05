be5d7d8d35c10a4a10aa03104e6fa8a0e74b1a87
Chris-Maina <krismaish34@gmail.com>
2017-09-05 16:09:58 +0300

    [Refactor]code

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) # pylint: disable=invalid-name
+    port = int(os.environ.get('PORT', 5000)) 
     app.run('', port=port)
     
